7ead3aba3bcddb04af64c31ca14913b019ddbc27
Jess Planck <jess@funroe.net>
2016-04-14 15:13:17 -0500

    Activate SASS compression

--- config.rb
+++ config.rb
@@ -19,8 +19,8 @@ sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "js"
 project_type = :stand_alone
-#output_style = :compressed
-output_style = :expanded
+output_style = :compressed
+#output_style = :expanded
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
@@ -42,7 +42,7 @@ asset_cache_buster :none
 
 # Make a copy of sprites with a name that has no uniqueness of the hash.
 on_sprite_saved do |filename|
-  if File.exists?(filename)    
+  if File.exists?(filename)
     FileUtils.mv filename, filename.gsub(%r{-s[a-z0-9]{10}\.png}, '.png').gsub('source/compass/library/', '')
   end
 end
@@ -68,22 +68,22 @@ end
 # rename_sprite module from comments at http://www.akawebdesign.com/2011/01/17/building-css-sprites-with-compasssass/
 #module Sass::Script::Functions
 #	require 'fileutils'
-#	
+#
 #	def rename_sprite(sprite_map, target_relative)
-#		
+#
 #		target_relative = target_relative.to_s
 #		target_relative.gsub!(/.*images\//,'')
 #		target_relative.gsub!(/"$/,'')
 #		sprite_map = sprite_map.to_s
 #		sprite_map["url('"] = ""
 #		sprite_map["')"] = ""
-#		
+#
 #		sass_absolute_path = File.dirname(__FILE__)
 #		sprite_absolute_path= File.join(sass_absolute_path, "library/images/sprites", File.basename(sprite_map))
 #		target_absolute_path= File.join(sass_absolute_path, "images", target_relative)
-#		
+#
 #		target_manual = File.join( Compass.configuration.css_path, "library/images/", target_relative)
-#		
+#
 #		FileUtils.mv(sprite_absolute_path, target_manual)
 #		return Sass::Script::Bool.new(true)
 #	end
