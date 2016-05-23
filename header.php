ea99b1cb08a1c5ce5cc1db8352c9c7061d699641
Jess Planck <jess@funroe.net>
2016-05-23 08:43:27 -0500

    Google webmaster tools site verification added to header!!

--- header.php
+++ header.php
@@ -16,6 +16,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<meta name="google-site-verification" content="m8Cj8r6-iwttNBQu4C-KGyXG13dMjahXqtU-LG0NT6c" />
 
 <?php wp_head(); ?>
 </head>
